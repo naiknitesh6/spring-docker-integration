88c46822a20c38e647a59e3b544d681636386754
naiknitesh6@gmail.com <naiknitesh6@gmail.com>
2022-07-24 15:16:33 +0530

    first commit

---
+++ src/main/java/com/firstspringboot/spring/Test.java
@@ -0,0 +1,26 @@
+package com.firstspringboot.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+//@Controller
+//@ResponseBody
+//public class Test 
+//{
+//	@RequestMapping("/test")
+//	public String Testing()
+//	{
+//	return "Connected0----)ewkqw";
+//	}
+//
+//}
+@RestController
+public class Test
+{@GetMapping("/home")
+	public String home()
+	{
+		return "Docker prokecy";
+	}
+}
