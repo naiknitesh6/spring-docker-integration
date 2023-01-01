5c818c62fd9ee60400606ea589ac4f3c55c3b097
naiknitesh6@gmail.com <naiknitesh6@gmail.com>
2023-01-01 17:51:36 +0530

    new update

--- src/main/java/com/firstspringboot/spring/Test.java
+++ src/main/java/com/firstspringboot/spring/Test.java
@@ -2,6 +2,7 @@ package com.firstspringboot.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 //}
 @RestController
 public class Test
-{@GetMapping("/home")
-	public String home()
+{@GetMapping("/home/{id}")
+	public String home(@PathVariable("id")int id)
 	{
-		return "Docker project";
+		return "Docker project"+id;
 	}
 }
